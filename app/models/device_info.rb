e26215c8f9a2098c629dc572d505ef40392ab8e6
Akshaykumar Chakkarkota <akshay.chakkarkota@cuelinks>
2022-05-06 00:48:53 +0530

    ruby code to send push notification

--- app/models/device_info.rb
+++ app/models/device_info.rb
@@ -1,3 +1,12 @@
 class DeviceInfo < ApplicationRecord
   belongs_to :user
+
+  def self.send_notification(tokens, payload, device_type='android')
+    messenger = (device_type == 'android' ? PushMessenger::Gcm.new : PushMessenger::Ios.new)
+    begin
+      messenger.deliver("#{device_type}_app", tokens, payload)
+    rescue Exception => error
+      Rails.logger.debug error
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_many :device_infos
+
+  def send_notification_to_user(title, description)
+    payload = {payload: { title: title , description: description } }
+    android_condition = "device_type = 'android' and user_id = #{id.to_i}"
+    ios_condition = "device_type = 'ios' and user_id = #{id.to_i}"
+    send_notification(payload, android_condition, 'android')
+    send_notification(payload, ios_condition, 'ios')
+  end
+
+  def send_notification(payload, condition, device_type)
+    tokens = DeviceInfo.where(condition).pluck(:device_token).compact
+      DeviceInfo.send_notification(tokens, payload, device_type)
+  end
 end
